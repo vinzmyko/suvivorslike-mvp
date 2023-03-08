c31423deed9e632e3247ab3320568fd37aa36c50
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2023-03-08 11:19:17 +0000

    v1.1 feat: implement player scene
    Added player scene with main scene and the player has movement code.

---
+++ scenes/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const MAX_SPEED: int = 200
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	var movement_vector: Vector2 = get_movement_vector()
+	var direction: Vector2 = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_movement_vector() -> Vector2:
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement) 
+

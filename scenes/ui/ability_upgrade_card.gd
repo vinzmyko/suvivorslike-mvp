1686e43868c9aff007a540b4a236ccb6caa61da8
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2023-03-13 20:05:48 +0000

    3.4 feat: animate upgrade cards

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -4,10 +4,13 @@ signal selected
 
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var disabled = false
 
 func _ready() -> void:
 	gui_input.connect(on_gui_input)
-
+	mouse_entered.connect(on_mouse_entered)
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
@@ -15,5 +18,36 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
+	
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()  
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
+
+
+func play_discard():
+	animation_player.play("discarded")
+
+
+func select_card():
+	disabled = true
+	animation_player.play("click")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await animation_player.animation_finished
+	selected.emit()
+
+
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")

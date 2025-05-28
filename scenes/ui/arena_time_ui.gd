16fd5327e7d600ddca138184eba06f4d0721e9b2
Vinz <vinzmykodelrosario@gmail.com>
2025-05-28 17:28:36 +0100

    feat: set up for survivors like mvp

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -11,6 +11,6 @@ func _process(delta: float) -> void:
 
 
 func format_seconds_to_string(seconds: float) -> String:
-	var minutes = floor(seconds / 60)
+	var minutes: int = floor(seconds / 60)
 	var remaining_seconds = floor(seconds - (minutes * 60))
 	return str(minutes) + ":" + ("%02d" % floor(remaining_seconds))

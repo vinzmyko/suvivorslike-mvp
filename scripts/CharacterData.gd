66e5e2401d295224bf47c2635e96d5655195cb8e
Vinz <vinzmykodelrosario@gmail.com>
2025-05-29 18:57:18 +0100

    feat: add CharacterData resource
    Resource which stores all the player information

---
+++ scripts/CharacterData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name CharacterData;
+
+@export var character_name: String;
+@export var sprite_texture: Texture2D;
+@export var starting_ability_controller: PackedScene;
+@export var max_speed: int;
+@export var acceleration: int;
+@export var max_health: int;

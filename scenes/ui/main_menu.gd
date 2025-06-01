5d9c1b230e59cbb74c848a563bef8cc42d162be3
Vinz <vinzmykodelrosario@gmail.com>
2025-06-01 14:22:43 +0100

    feat: adjust text sizes for new font

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -9,7 +9,6 @@ func _ready() -> void:
 	%UpgradeButton.pressed.connect(on_upgrade_button_pressed)
 
 func on_play_button_pressed() -> void:
-	#get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	get_tree().change_scene_to_file("res://scenes/ui/character_select.tscn")
 
 
